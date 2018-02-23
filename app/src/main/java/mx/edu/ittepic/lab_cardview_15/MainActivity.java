87f5dc96c2f49b5f2737637b0df4edf5d28368fb
name <carobox96@gmail.com>
2018-02-23 13:28:45 -0700

    commit

--- app/src/main/java/mx/edu/ittepic/lab_cardview_15/MainActivity.java
+++ app/src/main/java/mx/edu/ittepic/lab_cardview_15/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.widget.AdapterView;
 
 import java.util.ArrayList;
-
+ 
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<musica> listaMusica;
